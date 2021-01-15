c7174b177c9fa119ee54ce038adde30a11618aa8
Kishan <kishan.rai99693@gmail.com>
2021-01-15 19:06:47 +0530

    real time face-mask detection

---
+++ Face Mask Detection/Face Mask Detection using openCV.py
@@ -0,0 +1,49 @@
+import cv2
+import os
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+import numpy as np
+ 
+cascPath = os.path.dirname(
+    cv2.__file__) + "/data/haarcascade_frontalface_alt2.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+model = load_model("mask_recog1.h5")
+ 
+video_capture = cv2.VideoCapture(0)
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(gray,
+                                         scaleFactor=1.1,
+                                         minNeighbors=5,
+                                         minSize=(60, 60),
+                                         flags=cv2.CASCADE_SCALE_IMAGE)
+    faces_list=[]
+    preds=[]
+    for (x, y, w, h) in faces:
+        face_frame = frame[y:y+h,x:x+w]
+        face_frame = cv2.cvtColor(face_frame, cv2.COLOR_BGR2RGB)
+        face_frame = cv2.resize(face_frame, (224, 224))
+        face_frame = img_to_array(face_frame)
+        face_frame = np.expand_dims(face_frame, axis=0)
+        face_frame =  preprocess_input(face_frame)
+        faces_list.append(face_frame)
+        if len(faces_list)>0:
+            preds = model.predict(faces_list)
+        for pred in preds:
+            (mask, withoutMask) = pred
+        label = "Mask" if mask > withoutMask else "No Mask"
+        color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+        label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+        cv2.putText(frame, label, (x, y- 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+ 
+        cv2.rectangle(frame, (x, y), (x + w, y + h),color, 2)
+        # Display the resulting frame
+    cv2.imshow('Video', frame)
+    if cv2.waitKey(1) &amp; 0xFF == ord('q'):
+        break
+video_capture.release()
+cv2.destroyAllWindows()
