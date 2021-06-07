a61ff40d592e62abdd7b5b4343b1d628d6c7f590
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2021-06-07 18:27:09 +0530

    YouTube Video Downloader Using Pytube

--- YouTube Video Downloader/main.py
+++ YouTube Video Downloader/main.py
@@ -1,17 +1,17 @@
 from pytube import YouTube
 
-url = "https://www.youtube.com/watch?v=1MBR39rTEs8"
+url = "https://www.youtube.com/watch?v=1MBR39rTEs8"    #Enter the url of the video you want to download
 
 video = YouTube(url)
 
-print(video.title)
+print(video.title)    #prints the title of the youtube video
 
-print(video.thumbnail_url)
+print(video.thumbnail_url)    #prints the link of the youtube thumbnail image
 
-print(video.views)
+print(video.views)    #prints the views of the video
 
-print(video.description)
+print(video.description)    #prints the description of the video
 
 video = video.streams.get_highest_resolution()
 
-video.download()
+video.download()    #downloads the video in your current directory
