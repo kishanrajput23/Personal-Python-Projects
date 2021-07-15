70a1213234cb718dccec787de2fc0d0b1e3f2966
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2021-07-15 17:03:50 +0530

    Printing Colored Text

---
+++ Colored Text/Colored_Text.py
@@ -0,0 +1,7 @@
+import colorama
+from colorama import Fore,Back,Style
+colorama.init()
+print(Fore.GREEN+"HELLO FRIENDS WELCOME TO OUR CODING BUDDIES CHANNEL")
+print(Back.BLUE+"Hi Friends my name Kritika")
+print(Fore.RED+Back.GREEN+"This Is Our Coding Buddies Channel")
+print(Style.BRIGHT+"Do Like Share And Subscribe")
