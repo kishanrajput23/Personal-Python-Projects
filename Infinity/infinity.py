6aa5c83dc4829d202613cac0eef8f6fc60af1b21
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-04-11 18:32:45 +0530

    Infinity Pattern

---
+++ Infinity/infinity.py
@@ -0,0 +1,21 @@
+import turtle
+from turtle import *
+
+bgcolor("black")
+color("red")
+speed(11)
+right(45)
+
+for i in range(150):
+	circle(30)
+	if 7<i<62:
+		left(5)
+        
+	if 80<i<133:
+		right(5)
+
+	if i<80:
+		forward(10)
+	else:
+		forward(5)
+turtle.done()
