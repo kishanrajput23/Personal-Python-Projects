214bb6089b2f68384b0df88a71e7343831fa30fe
Pranshu_Singh <pranshusingh5123@gmail.com>
2023-10-23 20:29:13 +0530

    Update main.py

--- Alarm Clock/main.py
+++ Alarm Clock/main.py
@@ -2,22 +2,30 @@ import os
 import datetime
 import time
 
-year, month, day = input("Enter the date = ").split("/")    #splitting input values by'/'
-
-hour, minutes, second = input("Enter the Time = ").split(":")    #splitting input values by':'
-
-shedule_date = datetime.date(int(day), int(month), int(year))    #converting input values into interger
-
-n = 1
-
-while n > 0:
+def get_user_input():
+    year, month, day = input("Enter the date (YYYY/MM/DD) = ").split("/")
+    hour, minutes, second = input("Enter the Time (HH:MM:SS) = ").split(":")
     
-    if time.localtime().tm_hour == int(hour) and time.localtime().tm_min == int(minutes) and time.localtime().tm_sec == int(second) and datetime.date.today() == shedule_date:
-        
-        os.startfile("C:\\Users\\KISHAN\\Music\\Chogada.mp3")    #Enter the path of a song or ringtone where it is placed in your system
-        
-        break
+    return int(year), int(month), int(day), int(hour), int(minutes), int(second)
+
+def schedule_notification(year, month, day, hour, minutes, second):
+    schedule_date = datetime.date(year, month, day)
     
-    else:
-        
-        n +=1
+    while True:
+        current_time = time.localtime()
+        if (current_time.tm_hour == hour and
+            current_time.tm_min == minutes and
+            current_time.tm_sec == second and
+            datetime.date.today() == schedule_date):
+            return True
+        time.sleep(1)
+    return False
+
+def play_notification():
+    os.startfile("C:\\Users\\KISHAN\\Music\\Chogada.mp3")  # User can replace with their file path
+
+if __name__ == "__main__":
+    year, month, day, hour, minutes, second = get_user_input()
+    
+    if schedule_notification(year, month, day, hour, minutes, second):
+        play_notification()
