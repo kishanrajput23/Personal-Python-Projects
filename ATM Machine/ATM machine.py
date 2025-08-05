f74919c1f72f34009f47fcbcd4dc6d2c200185bd
sushantgiri34 <sushantgoswami34@emailcom>
2025-08-05 17:05:31 +0530

    Improved ATM Machine functionality

--- ATM Machine/ATM machine.py
+++ ATM Machine/ATM machine.py
@@ -1,33 +1,35 @@
 balance = 1000
-print("""
-Welcome to ATM Machine
 
-Choose Transaction
+while True:
+    print("Choose a transaction:")
+    print("1. Balance Money")
+    print("2. Withdraw Money")
+    print("3. Deposit Balance")
+    print("4. Exit")
+    
+    option = int(input("Enter The Task Transaction (1/2/3/4): "))
 
-1)BALANCE
-2)WITHDRAW
-3)DEPOSIT
-4)EXIT
+    if(option == 1):
+        print("Your balance is ", balance)
 
-""")
-option = int(input("Enter Transaction "))
-
-if(option == 1):
-    print("Your balance is ", balance)
-elif(option==2):
-    withdraw = float(input("Enter amount to withdraw "))
-    if(balance > withdraw):
-        total = balance - withdraw
+    elif(option==2):
+        withdraw = float(input("Enter amount to withdraw "))
+        if(balance > withdraw):
+            total = balance - withdraw
+            print("success")
+            print("your new balance is :",total)
+        else:
+            print("insufficient Balance")
+    
+    elif(option==3):
+        deposit = float(input("Enter amount to deposit "))
+        totalbalance = balance + deposit
         print("success")
-        print("your new balance is :",total)
+        print("total balnace now is: ", totalbalance)
+    
+    elif(option==4):
+        print("thanks for choosing us!")
+        break
+    
     else:
-        print("insufficient Balance")
-elif(option==3):
-    deposit = float(input("Enter amount to deposit "))
-    totalbalance = balance + deposit
-    print("success")
-    print("total balnace now is: ", totalbalance)
-elif(option==4):
-    exit()
-else:
-    print("no selected transaction")
+        print("no selected transaction")
